b78e6631c76092ddcc68ce80303490e95c77ff57
lucas <mr.pine97@gmail.com>
2021-08-31 13:52:24 -0300

    fixaçao1

---
+++ components/fixation-exercise/src/App.js
@@ -0,0 +1,10 @@
+import Image from './Image';
+import './App.css';
+
+function App() {
+  return (
+    <Image source="https://i.gifer.com/ReYG.gif" alternativeText="Get Schwifty" />
+  );
+}
+
+export default App;
