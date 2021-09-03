abe99bbed6bc0652ce6176841b113e113459ffe3
lucas <mr.pine97@gmail.com>
2021-09-03 18:30:23 -0300

    impl: Header

--- my-pokedex/src/App.js
+++ my-pokedex/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-pokedex/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header class="header navbar fixed-top navbar-light">
+        <h1>
+          pokedex
+        </h1>
+      </header>
+    )
+  }
+}
